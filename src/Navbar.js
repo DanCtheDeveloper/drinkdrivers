7bb7bc9dce0ace59e2f12e0b616b2bf72968a917
AndreTaq <andretaqian@gmail.com>
2023-10-21 13:39:14 -0700

    update images file

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,15 +3,15 @@ export default function Navbar() {
     return(
         <nav className="nav">
             <Link to="/cart">
-                <img src={ require("./images/cart-logo.png")} alt="Cart" height={ 200 } width={ 200 }/>
+                <img src={ require("./components/images/Cart.png")} alt="Cart" height={ 200 } width={ 200 }/>
             </Link>
             <Link to="/" className="drink-drivers">
-                {/* <img src={ require("/images/app-logo.png")} alt="DRINK DRIVERS" height={ 200 } width={ 200 }/> */}
+                <img src={ require("./components/images/App-logo.png")} alt="DRINK DRIVERS" height={ 200 } width={ 200 }/>
             </Link>
             <ul>
                 <li>
                     <Link to="/login">
-                        {/* <img src={ require("/images/settings-logo.png")} alt="Login" height={ 200 } width={ 200 }/> */}
+                        <img src={ require("./components/images/Settings-logo.png")} alt="Login" height={ 200 } width={ 200 }/>
                     </Link>
                 </li>
             </ul>
