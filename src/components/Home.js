1bb2026eacf0d6c96f00e023ad9c2fc2065122b8
nicolas572 <nicolas572@yahoo.com>
2023-10-23 12:33:54 -0700

    pulling from main, adding onto home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ export default function Home() {
     return (
         <div>
             <h1>Home</h1>
+            <img src="./components/images/Welcome.png" alt="Welcome"/>
         </div>
     );
 }
