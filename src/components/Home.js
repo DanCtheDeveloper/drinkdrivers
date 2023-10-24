1d9b270b2ceb78083069d8859c1a395c0418e1e7
nicolas572 <nicolas572@yahoo.com>
2023-10-23 19:04:30 -0700

    updating home page and adding css file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,25 @@
+import { Link } from "react-router-dom"
+
 export default function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <img src="./components/images/Welcome.png" alt="Welcome"/>
+            <img src="./images/Welcome.png" alt="Welcome"/>
+            <div>
+                <Link to={'/vendors'}>
+                    <button>View Vendors</button>
+                </Link>
+                <Link to={'/brews'}>
+                    <button>House Brews</button>
+                </Link>
+                <Link to={'/about'}>
+                    <button>About</button>
+                </Link>
+                <Link to={'/signup'}>
+                    <button>Don't have an account? Sign up here!</button>
+                </Link>
+            </div>
+
         </div>
     );
 }
